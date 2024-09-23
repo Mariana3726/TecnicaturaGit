dfa6ee794d9727c6619145ed24644be3c5d8f4c3
Mariana Aguilera <marian5174@gmail.com>
2024-09-22 22:27:38 -0300

    Clase Objetos en Java

--- Java/Leccion3/LeccionClasesObjetos/src/Clases/Persona.java
+++ Java/Leccion3/LeccionClasesObjetos/src/Clases/Persona.java
@@ -7,4 +7,13 @@ public class Persona {
     String nombre;
     String apellido;
     
+    //Métododos de la clase (Acciones)
+    //public: es para indicar que el método se puede utilizar fuera de esta clase
+    //void: indica que no regresa ningún tipo de información
+    //una clase es nuestra plantilla
+    public void obtenerInformacion(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+    }
+    
 }
